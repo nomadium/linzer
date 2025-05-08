3bfb48611c8168348b9a2aed4086a5f1c1b187fe
Miguel Landaeta <miguel@miguel.cc>
2025-05-08 16:26:46 +0100

    wip

--- lib/linzer/message/adapter/http_gem/request.rb
+++ lib/linzer/message/adapter/http_gem/request.rb
@@ -4,49 +4,16 @@ module Linzer
   class Message
     module Adapter
       module HTTPGem
-        class Request < Abstract
-          def initialize(operation, **options)
-            @operation = operation
-            freeze
-          end
-
+        class Request < Linzer::Message::Adapter::NetHTTP::Request
           def headers
             @operation.headers
-          end
-
-          def attach!(signature)
-            signature.to_h.each { |h, v| @operation[h] = v }
-            @operation
           end
 
           private
 
           def derived(name)
-            case name.value
-            when :method           then @operation.verb.to_s.upcase
-            when :"target-uri"     then @operation.uri.to_s
-            when :authority        then @operation.uri.authority.downcase
-            when :scheme           then @operation.uri.scheme.downcase
-            when :"request-target" then @operation.uri.request_uri
-            when :path             then @operation.uri.path
-            when :query            then "?%s" % String(@operation.uri.query)
-            when :"query-param"    then query_param(name)
-            end
-          end
-
-          def query_param(name)
-            param_name = name.parameters["name"]
-            return nil if !param_name
-            decoded_param_name = URI.decode_uri_component(param_name)
-            params = CGI.parse(@operation.uri.query)
-            URI.encode_uri_component(params[decoded_param_name]&.first)
-          end
-
-          def field(name)
-            has_tr = name.parameters["tr"]
-            return nil if has_tr # HTTP requests don't have trailer fields
-            value = @operation[name.value.to_s]
-            value.dup&.strip
+            return @operation.verb.to_s.upcase if name.value == :method
+            super(name)
           end
         end
       end
