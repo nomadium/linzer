a0a971cf94ba99882358aaf801fda8cb50855630
Miguel Landaeta <miguel@miguel.cc>
2025-05-17 21:28:55 +0100

    Small refactor on how signature verification exceptions are raised

--- lib/linzer/verifier.rb
+++ lib/linzer/verifier.rb
@@ -19,26 +19,27 @@ module Linzer
       private
 
       def validate(message, key, signature, no_older_than: nil)
-        raise Error.new "Message to verify cannot be null"       if message.nil?
-        raise Error.new "Key to verify signature cannot be null" if key.nil?
-        raise Error.new "Signature to verify cannot be null"     if signature.nil?
+        raise Error, "Message to verify cannot be null"       if message.nil?
+        raise Error, "Key to verify signature cannot be null" if key.nil?
+        raise Error, "Signature to verify cannot be null"     if signature.nil?
 
         if !signature.respond_to?(:value) || !signature.respond_to?(:components)
-          raise Error.new "Signature is invalid"
+          raise Error, "Signature is invalid"
         end
 
-        raise Error.new "Signature raw value to cannot be null" if signature.value.nil?
-        raise Error.new "Components cannot be null"             if signature.components.nil?
+        raise Error, "Signature raw value to cannot be null" if signature.value.nil?
+        raise Error, "Components cannot be null"             if signature.components.nil?
 
         validate_components message, signature.components
 
         return unless no_older_than
-        raise Error.new "Signature created more than #{no_older_than} seconds ago" if signature.older_than?(no_older_than.to_i)
+        old_sig_msg = "Signature created more than #{no_older_than} seconds ago"
+        raise Error, old_sig_msg if signature.older_than?(no_older_than.to_i)
       end
 
       def verify_or_fail(key, signature, data)
         return true if key.verify(signature, data)
-        raise Error.new "Failed to verify message: Invalid signature."
+        raise Error, "Failed to verify message: Invalid signature."
       end
     end
   end
