nomadium/linzer

commit d83dd16083a060b71902ccd4c4abc13bdc4b03a7

Do not convert headers to a hash
Instead make use of whatever mechanism the existing libraries
provider to access a single header by name, regardless of the
notation.

This also changes how `rack` response headers are handled. This
should now more closely resemble request headers.

lib/linzer/message/adapter/rack/common.rb
@@ -74,11 +74,9 @@ module Linzer
             if has_tr
               value = tr(name)
             else
-              if request?
-                rack_header_name = rack_header_name(name.value.to_s)
-                value = @operation.env[rack_header_name]
-              end
-              value = @operation.headers[name.value.to_s] if response?
+              rack_header_name = rack_header_name(name.value.to_s)
+              value = @operation.env[rack_header_name] if request?
+              value = @operation.get_header(rack_header_name) if response?
             end
             value.dup&.strip
           end

lib/linzer/message/adapter/rack/response.rb
@@ -16,12 +16,14 @@ module Linzer
             freeze
           end
 
-          def headers
-            @operation.headers
+          def header(name)
+            @operation.get_header(rack_header_name(name))
           end
 
           def attach!(signature)
-            signature.to_h.each { |h, v| @operation[h] = v }
+            signature.to_h.each do |h, v|
+              @operation.set_header(rack_header_name(h), v)
+            end
             @operation
           end
         end

spec/rack_helper.rb
@@ -2,7 +2,7 @@
 
 module Linzer
   module Test
-    module Request
+    module RackHelper
       extend self
 
       def new_request(verb, path = "/", params = {}, headers = {})
@@ -17,6 +17,10 @@ module Linzer
         }
 
         Rack::Request.new(build_rack_env(headers).merge(args))
+      end
+
+      def new_response(body = nil, status = 200, headers = {})
+        Rack::Response.new(body, status, build_rack_env(headers))
       end
 
       private

lib/linzer/message/adapter/http_gem/response.rb
@@ -14,8 +14,8 @@ module Linzer
             freeze
           end
 
-          def headers
-            @operation.headers
+          def header(name)
+            @operation[name]
           end
 
           # XXX: this implementation is incomplete, e.g.: ;tr parameter is not supported yet

lib/linzer/message/adapter/net_http/response.rb
@@ -13,8 +13,8 @@ module Linzer
             freeze
           end
 
-          def headers
-            @operation.each_header.to_h
+          def header(name)
+            @operation[name]
           end
 
           # XXX: this implementation is incomplete, e.g.: ;tr parameter is not supported yet

lib/linzer.rb
@@ -59,7 +59,12 @@ module Linzer
 
     def verify!(request_or_response, key: nil, no_older_than: 900)
       message = Message.new(request_or_response)
-      signature = Signature.build(message.headers.slice("signature", "signature-input"))
+      signature_headers = {}
+      %w(signature-input signature).each do |name|
+        value = message.header(name)
+        signature_headers[name] = value if value
+      end
+      signature = Signature.build(signature_headers)
       keyid = signature.parameters["keyid"]
       raise Linzer::Error, "key not found" if !key && !keyid
       verify_key = block_given? ? (yield keyid) : key

lib/linzer/message.rb
@@ -15,7 +15,7 @@ module Linzer
     def_delegators :@adapter, :request?, :response?, :attached_request?
 
     # fields look up
-    def_delegators :@adapter, :headers, :field?, :[]
+    def_delegators :@adapter, :header, :field?, :[]
 
     # to attach a signature to the underlying HTTP message
     def_delegators :@adapter, :attach!

lib/linzer/message/adapter/abstract.rb
@@ -37,7 +37,7 @@ module Linzer
           end
         end
 
-        def headers
+        def header(name)
           raise Linzer::Error, "Sub-classes are required to implement this method!"
         end
 

commit 9d5d5d64692d2dae9647898d0a81db5b68a35c20

Fix a few bugs when signing rack responses
The regression was introduced by mistake in d83dd160.

lib/linzer/message/adapter/rack/common.rb
@@ -63,7 +63,7 @@ module Linzer
             else
               rack_header_name = rack_header_name(name.value.to_s)
               value = @operation.env[rack_header_name] if request?
-              value = @operation.get_header(rack_header_name) if response?
+              value = @operation.get_header(name.value.to_s) if response?
             end
             value.dup&.strip
           end

lib/linzer/message/adapter/rack/response.rb
@@ -17,12 +17,12 @@ module Linzer
           end
 
           def header(name)
-            @operation.get_header(rack_header_name(name))
+            @operation.get_header(name)
           end
 
           def attach!(signature)
             signature.to_h.each do |h, v|
-              @operation.set_header(rack_header_name(h), v)
+              @operation.set_header(h, v)
             end
             @operation
           end

spec/rack_helper.rb
@@ -20,7 +20,7 @@ module Linzer
       end
 
       def new_response(body = nil, status = 200, headers = {})
-        Rack::Response.new(body, status, build_rack_env(headers))
+        Rack::Response.new(body, status, headers.transform_values(&:to_s))
       end
 
       private