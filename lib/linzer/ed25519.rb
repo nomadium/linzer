efdf1fd3b30c937339c8a58cd5df4c865b0e96fd
Miguel Landaeta <miguel@miguel.cc>
2025-06-03 23:01:59 +0100

    Add validation routines on Ed25519 key operations

--- lib/linzer/ed25519.rb
+++ lib/linzer/ed25519.rb
@@ -4,11 +4,21 @@ module Linzer
   module Ed25519
     class Key < Linzer::Key
       def sign(data)
+        validate_signing_key
         material.sign(nil, data)
       end
 
       def verify(signature, data)
+        validate_verify_key
         material.verify(nil, signature, data)
+      end
+
+      def public?
+        has_pem_public?
+      end
+
+      def private?
+        has_pem_private?
       end
     end
   end
