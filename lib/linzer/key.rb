5e2c2036bdaca0425910208cba21602e3ecef460
Miguel Landaeta <miguel@miguel.cc>
2025-06-06 23:10:43 +0100

    Address pending comment on missing validation on keys
    Add validation routines on key instances before performing
    signing and verifying operations.

--- lib/linzer/key.rb
+++ lib/linzer/key.rb
@@ -25,6 +25,14 @@ module Linzer
       raise Error, abstract_error
     end
 
+    def public?
+      material.public?
+    end
+
+    def private?
+      material.private?
+    end
+
     private
 
     def validate
@@ -35,6 +43,24 @@ module Linzer
       no_digest = !@params.key?(:digest) || @params[:digest].nil? || String(@params[:digest]).empty?
       no_digest_error = "Invalid key definition, no digest algorithm was selected."
       raise Error, no_digest_error if no_digest
+    end
+
+    def validate_signing_key
+      raise SigningError, "Private key is needed!" unless private?
+    end
+
+    def validate_verify_key
+      raise VerifyError, "Public key is needed!" unless public?
+    end
+
+    def has_pem_public?
+      material.public_to_pem.match?(/^-----BEGIN PUBLIC KEY-----/)
+    end
+
+    def has_pem_private?
+      material.private_to_pem.match?(/^-----BEGIN PRIVATE KEY-----/)
+    rescue
+      false
     end
   end
 end
