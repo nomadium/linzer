9f0124b08dd088f0317293d26dd81c702025d46d
Miguel Landaeta <miguel@miguel.cc>
2026-05-17 15:24:36 +0100

    Support signing profiles in bundled HTTP client
    Pass optional signing profile through the built-in HTTP client so
    it can influence signature generation via Linzer.sign!.

--- lib/linzer/http.rb
+++ lib/linzer/http.rb
@@ -104,9 +104,10 @@ module Linzer
       params     = options[:params] || {}
 
       Linzer.sign! request,
-                   key: key,
+                   key:        key,
                    components: components,
-                   params: params
+                   params:     params,
+                   profile:    options[:profile]
 
       signature_headers  = request.each_header.to_h.slice("signature-input", "signature")
 
