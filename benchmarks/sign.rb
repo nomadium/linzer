5d16624fd26ddad3869c84b0d323eb88ca1a30c7
Miguel Landaeta <miguel@miguel.cc>
2026-08-25 21:23:36 +0000

    Add ML-DSA benchmarks for both backends

--- benchmarks/sign.rb
+++ benchmarks/sign.rb
@@ -1,5 +1,7 @@
 require "benchmark_driver"
 require "openssl"
+require "linzer"
+require "linzer/ml_dsa"
 
 BENCH_ALGS = %i[
   ed25519
@@ -9,6 +11,8 @@ BENCH_ALGS = %i[
   rsa_pss_sha512
   hmac_sha256
   jws_ed25519
+  ml_dsa_44_openssl
+  ml_dsa_44_ml_dsa
 ]
 
 puts "Linzer.sign! benchmark — supported algorithms"
@@ -19,11 +23,13 @@ puts Time.now.utc
 
 BENCH_ALGS.each do |alg|
   next if alg == :rsa_pss_sha512 && RUBY_VERSION < "3.1.0"
+  next if alg == :ml_dsa_44_openssl && !Linzer::MLDSA.openssl_supported?("ml-dsa-44")
   Benchmark.driver do |x|
     x.prelude <<~RUBY
       require "bundler/setup"
       require "linzer"
       require "linzer/jws"
+      require "linzer/ml_dsa"
 
       def generate_linzer_key(alg)
         case alg
@@ -33,6 +39,10 @@ BENCH_ALGS.each do |alg|
           Linzer.public_send(:"generate_#{alg}_key", 2048, "bench-#{alg}")
         when :jws_ed25519
           Linzer.generate_jws_key(algorithm: "EdDSA")
+        when :ml_dsa_44_openssl
+          Linzer.generate_ml_dsa_44_key("bench-#{alg}", backend: :openssl)
+        when :ml_dsa_44_ml_dsa
+          Linzer.generate_ml_dsa_44_key("bench-#{alg}", backend: :ml_dsa)
         else
           raise ArgumentError, "Unknown algorithm!"
         end
